a5402d88ab5f39d8f1ab86a52984d2a12b1a25b8
Jane Smith <example@email.com>
2018-12-04 18:38:23 -0800

    react app is working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,25 +3,34 @@ import superagent from 'superagent';
 import Header from './components/header.js';
 import Page from './components/page.js';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: [],
+      title: 'Katherine Smith',
+    };
+  }
+
+  componentDidMount() {
+    superagent('get', 'https://full-stack-project-web-server.herokuapp.com/content')
+      .then(results => {
+        this.setState({ content: results.body });
+      })
+      .catch(console.error);
+  }
+
   render() {
+    const navLinks = this.state.content.map((obj,idx) =>
+      <a key={'deck'+(idx+1)} href={'#deck'+(idx+1)}>{obj.deckTitle}</a>);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Header title={this.state.title}>
+          {navLinks}
+        </Header>
+        <Page content={this.state.content}/>
+      </React.Fragment>
     );
   }
 }
--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../styles/header.scss';
+import styles from '../styles/header.modules.scss';
 
 class Header extends React.Component {
   render() {
